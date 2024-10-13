bb1685b49e8790bedbf65e12490f413798380e28
Piter Capistrano <pitercapistrano@gmail.com>
2024-10-13 16:53:48 -0300

    Criação do Item de Lista Pedido

---
+++ app/src/main/java/com/pitercapistrano/applojavirtualadm/model/Pedido.kt
@@ -0,0 +1,11 @@
+package com.pitercapistrano.applojavirtualadm.model
+
+data class Pedido(
+    val endereco: String? = null,
+    val telefone: String? = null,
+    val produto: String? = null,
+    val preco: String? = null,
+    val tamanho_calcado: String? = null,
+    val status_pagamento: String? = null,
+    val status_entrega: String? = null
+)
